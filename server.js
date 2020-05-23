ed0d471eb136a010c97b7b15a73b4436b627405b
Pedro Machado <machadop1407@gmail.com>
2020-05-23 20:14:57 -0300

    news

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+//NEWS API
+const NewsAPI = require('newsapi')
+const newsapi = new NewsAPI('046b7a8dcf034c39a8dc468583bd0253')
+
+
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index')
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var weekAgo = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate() - 7)
+    newsapi.v2.topHeadlines({
+        q: 'covid-19',
+        category: 'business',
+        language: 'en',
+        from: weekAgo,
+        to: date,
+    }).then(response => {
+        console.log(response);
+        res.render('index', {articles: response.articles})
+    });
+    
 })
 
 app.get('/volunteer', (req, res) => {
