cdcb468b513127cf9bd4554dabfb79f2ce18baf6
Pedro Machado <machadop1407@gmail.com>
2020-05-23 19:19:15 -0300

    changed css

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/news', (req, res) => {
     res.render('index')
 })
 
