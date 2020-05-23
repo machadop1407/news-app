1ba913e6f84811566f55c100a32976b9af9c9bf8
Pedro Machado <machadop1407@gmail.com>
2020-05-23 19:48:51 -0300

    ui for news page

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index')
