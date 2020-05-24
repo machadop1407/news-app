ce1dac4ae5b7250b8706011264470c83735ab59a
Pedro Machado <machadop1407@gmail.com>
2020-05-24 09:25:19 -0300

    finished volunteer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 
 //NEWS API
@@ -10,7 +11,8 @@ const newsapi = new NewsAPI('046b7a8dcf034c39a8dc468583bd0253')
 const GSR = require('google-search-results-nodejs')
 let client = new GSR.GoogleSearchResults("d1c96b22c28a88b9a1815f0a604e7899876d186f80a5b6d4f97371665f54f68a")
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -33,24 +35,30 @@ app.get('/', (req, res) => {
     res.render('index', {articles: [{url: "sss", title: "snsns", description: "cacabb", author: "iiiii", urlToImage: "llll", publishedAt: "vishal"}]})
 })
 
-app.get('/volunteer', (req, res) => {
+app.post('/searchVolunteer', (req, res) => {
+
+    var country = req.body.location
     var parameter = {
         engine: "google_jobs",
         q: "barista",
         google_domain: "google.com",
         gl: "ca",
         hl: "en",
-        location: "United States",
+        location: country,
         
     };
     
     var callback = function(data) {
-      res.render('volunteer', {jobs: data.jobs_results})
+      res.send({jobs: data.jobs_results})
     }
     
     // Show result as JSON
     client.json(parameter, callback)
-        //res.render('volunteer')
+    
+})
+
+app.get('/volunteer', (req, res) => {
+        res.render('volunteer')
     })
     
     app.get('/donate', (req, res) => {
