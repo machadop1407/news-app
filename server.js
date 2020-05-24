3f967c8fcdf7ae48bbff601b0a76a4b997d4239f
Pedro Machado <machadop1407@gmail.com>
2020-05-23 22:22:56 -0300

    volunteer

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const newsapi = new NewsAPI('15e9098a61a3b7915a99983e1df5ae70aeedc89446ecd1e6318
 
 //GOOGLE API
 const GSR = require('google-search-results-nodejs')
-let client = new GSR.GoogleSearchResults()
-
-
-const GSR = require('google-search-results-nodejs')
 let client = new GSR.GoogleSearchResults("d1c96b22c28a88b9a1815f0a604e7899876d186f80a5b6d4f97371665f54f68a")
 
 
@@ -35,50 +31,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/volunteer', (req, res) => {
-<<<<<<< HEAD
     var parameter = {
         engine: "google_jobs",
         q: "barista",
         google_domain: "google.com",
         gl: "ca",
         hl: "en",
+        location: "Canada",
+        
     };
     
     var callback = function(data) {
-      console.log(data)
+      res.render('volunteer', {jobs: data.jobs_results})
     }
     
     // Show result as JSON
     client.json(parameter, callback)
-    res.render('volunteer')
-=======
-   
-var parameter = {
-    engine: "google_jobs",
-    q: "barista",
-    google_domain: "google.com",
-    gl: "ca",
-    hl: "en",
-    location: "Canada",
+        //res.render('volunteer')
+    })
     
-};
-
-var callback = function(data) {
-  console.log(data)
-  //res.json(data)
-}
-
-// Show result as JSON
-client.json(parameter, callback)
-    //res.render('volunteer')
->>>>>>> a5cb72ff8ba68f735c1339aef474cd519648eb56
-})
-
-app.get('/donate', (req, res) => {
-    res.render('donate')
-})
-
+    app.get('/donate', (req, res) => {
+        res.render('donate')
+    })
 app.listen(process.env.PORT || 6969)
 
 module.exports = app
-
